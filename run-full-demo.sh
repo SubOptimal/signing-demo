1ba1ead7479aa3c240b65fb343037772b38d313c
Frank Dietrich <653288+SubOptimal@users.noreply.github.com>
2025-08-01 22:57:48 +0200

    Amends the demo execution script.

--- run-full-demo.sh
+++ run-full-demo.sh
@@ -1,53 +1,103 @@
 #!/bin/bash
 
+# The script processes all required steps to set up and validate SSH signed commits locally.
+
 set -o errexit
 
-[ ! -f john_doe ] && ssh-keygen -t ed25519 -C "john.doe@example.com" -f john_doe -P ""
-[ ! -d signing-demo ] && git clone git@github.com:SubOptimal/signing-demo.git
-[ ! -f web-flow.gpg ] && curl -sO https://github.com/web-flow.gpg
+work_dir="$(realpath signing-work-dir)"
+demo_repo_dir="${work_dir}/demo-repo"
 
+if [[ "$1" = "--cleanup" ]]; then
+  echo "[*] Cleanup the working directory"
+  rm -rfi "${work_dir}"
+  exit
+fi
+
+echo "[*] === Preparation steps ==="
+mkdir -p "${work_dir}"
+
+if ! [[ -f "${work_dir}/john_doe" ]]; then
+  echo "[+] Create a SSH key pair for the demo user John Doe"
+  ssh-keygen -q -t ed25519 -C "john.doe@example.com" -f "${work_dir}/john_doe" -P ""
+else
+  echo "[-] Skipped: SSH key pair for the demo user John Doe already exist"
+fi
+
+if [[ -d "${demo_repo_dir}" ]]; then
+  cat <<HERE
+WARNING: The demo repository seems to exist already.
+         ${demo_repo_dir}
+
+To avoid data loss or unexpected behaviour the further execution is stopped at this point.
+
+If you want to rerun all steps call the script with the parameter '--cleanup'. This will
+remove the sub-directory ${work_dir} recursively and all data in it.
+HERE
+  exit 1
+fi
+
+echo "[+] Create a clone of this repository in the working directory"
+git clone --quiet "$(pwd)" "${demo_repo_dir}"
+
+# to avoid repeated downloads after a cleanup, the file is intentionally not downloaded
+# into the working directory
+if ! [[ -f web-flow.gpg ]]; then
+  echo "[+] Download the GitHub GPG public signing key"
+  curl -sO https://github.com/web-flow.gpg
+echo
+  echo "[-] Skipped: GitHub GPG public signing key already downloaded"
+fi
+
+echo "[*] === Configure demo repository for SSH signing ==="
+cd "${demo_repo_dir}"
+# sign all commits and tags by default
+git config commit.gpgsign true
+git config tag.gpgsign true
+# use SSH keys for signing
+git config gpg.format ssh
+# configure the SSH key to use for signing
+git config user.signingkey "$(cut -d ' ' -f 1,2 < ../john_doe.pub)"
+# the email address must match the one used for the key pair
+git config user.email "john.doe@example.com"
+git config user.name "John Doe"
+
+echo "[*] Commit with John's SSH key"
+cd "${work_dir}"
+# for the demo a dedicated SSH agent is used to avoid interference with an already running agent
+eval "$(ssh-agent)"
+# the used SSH agent needs to know about John's private key
+ssh-add john_doe
+cd "${demo_repo_dir}"
+# do some change and commit it as John
+echo "signing john" >> README.md
+git add README.md
+git commit --quiet --message "Signed by John."
+# only needed for the demo agent
+kill -s 9 ${SSH_AGENT_PID}
+
+echo "[*] Git log show signature: with allowed signers file not configured"
+# error: gpg.ssh.allowedSignersFile needs to be configured and exist for ssh signature verification
+cd "${demo_repo_dir}"
+git log --show-signature -2
+
+echo "[*] Configure allowed signers file in the repository"
+cd "${work_dir}"
 allowed_signers_file="$(realpath allowed_signers)"
 true > "${allowed_signers_file}"
+cd "${demo_repo_dir}"
+git config --local gpg.ssh.allowedSignersFile "${allowed_signers_file}"
 
-git config --global gpg.ssh.allowedSignersFile "${allowed_signers_file}"
+echo "[*] Git log show signature: with allowed signers file configured"
+# Good "git" signature with ED25519 key SHA256:...
+# No principal matched.
+cd "${demo_repo_dir}"
+git log --show-signature -2
 
-(
-  echo "[*] Prepare John's repository"
-  rm -rf signing-john
-  git clone signing-demo signing-john
-  cd signing-john
-  git config commit.gpgsign true
-  git config tag.gpgsign true
-  git config gpg.format ssh
-  git config user.signingkey "$(cut -d ' ' -f 1,2 < ../john_doe.pub)"
-  git config user.name "John Doe"
-  git config user.email "john.doe@example.com"
-)
+echo "[*] Add John's public key to the allowed signers file"
+cd "${work_dir}"
+awk '{ print $3,$1,$2 }' < john_doe.pub > "${allowed_signers_file}"
 
-(
-  echo "[*] Commit with John's SSH key"
-  eval "$(ssh-agent)"
-  ssh-add john_doe
-  cd signing-john
-  echo "signing john" >> README.md
-  git add README.md
-  git commit --quiet --message "Signed by John."
-  kill -s 9 $SSH_AGENT_PID
-)
-
-log_file="$(realpath signing-john.log)"
-true > "${log_file}"
-(
-  echo "[*] Signing log - missing John's signature"
-  cd signing-john
-  git log --show-signature -2
-) |& tee -a "${log_file}"
-
-echo "[*] Add John's public key to allowed signers"
-awk '{ print $3,$1,$2 }' < john_doe.pub > allowed_signers
-
-(
-  echo "[*] Signing log - with John as allowed signer"
-  cd signing-john
-  git log --show-signature -2
-) |& tee -a "${log_file}"The ""
+echo "[*] Git log show signature: with John's public key added to the allowed signers file"
+# Good "git" signature for john.doe@example.com with ED25519 key SHA256:...
+cd "${demo_repo_dir}"
+git log --show-signature -2
