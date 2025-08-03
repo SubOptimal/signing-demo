b3421c3422e9b41e72dec63e428565ee1637242c
Frank Dietrich <653288+SubOptimal@users.noreply.github.com>
2025-08-03 18:10:05 +0200

    Adds steps for GitHub signed commits.
    - show the Git log without the GitHub signing key
    - adds the GitHub GPG public key
    - show the Git log with the GitHub signing key

--- run-full-demo.sh
+++ run-full-demo.sh
@@ -4,6 +4,7 @@
 
 set -o errexit
 
+repo_root_dir="$(pwd)"
 work_dir="$(realpath signing-work-dir)"
 demo_repo_dir="${work_dir}/demo-repo"
 
@@ -40,7 +41,7 @@ WARNING: The demo repository seems to exist already.
 To avoid data loss or unexpected behaviour the further execution is stopped at this point.
 
 If you want to rerun all steps call the script with the parameter '--cleanup'. This will
-remove the sub-directory ${work_dir} recursively and all data in it.
+remove the sub-directory ./$(basename $work_dir) recursively and all data in it.
 HERE
   exit 1
 fi
@@ -57,7 +58,7 @@ echo
   echo "[-] Skipped: GitHub GPG public signing key already downloaded"
 fi
 
-echo "[*] === Configure demo repository for SSH signing ==="
+echo "[+] Configure demo repository for SSH signing"
 cd "${demo_repo_dir}"
 # sign all commits and tags by default
 git config commit.gpgsign true
@@ -70,7 +71,8 @@ git config user.signingkey "$(cut -d ' ' -f 1,2 < ../john_doe.pub)"
 git config user.email "john.doe@example.com"
 git config user.name "John Doe"
 
-echo "[*] Commit with John's SSH key"
+echo "[*] === Process Git actions ==="
+echo "[+] Commit with John's SSH key"
 cd "${work_dir}"
 # for the demo a dedicated SSH agent is used to avoid interference with an already running agent
 eval "$(ssh-agent)"
@@ -84,29 +86,50 @@ git commit --quiet --message "Signed by John."
 # only needed for the demo agent
 kill -s 9 ${SSH_AGENT_PID}
 
-echo "[*] Git log show signature: with allowed signers file not configured"
+echo "[+] Git log show signature: with allowed signers file not configured"
 # error: gpg.ssh.allowedSignersFile needs to be configured and exist for ssh signature verification
 cd "${demo_repo_dir}"
 git log --show-signature -2
 
-echo "[*] Configure allowed signers file in the repository"
+echo "[+] Configure allowed signers file in the repository"
 cd "${work_dir}"
 allowed_signers_file="$(realpath allowed_signers)"
 true > "${allowed_signers_file}"
 cd "${demo_repo_dir}"
 git config --local gpg.ssh.allowedSignersFile "${allowed_signers_file}"
 
-echo "[*] Git log show signature: with allowed signers file configured"
+echo "[+] Git log show signature: with allowed signers file configured"
 # Good "git" signature with ED25519 key SHA256:...
 # No principal matched.
 cd "${demo_repo_dir}"
 git log --show-signature -2
 
-echo "[*] Add John's public key to the allowed signers file"
+echo "[+] Add John's public key to the allowed signers file"
 cd "${work_dir}"
 awk '{ print $3,$1,$2 }' < john_doe.pub > "${allowed_signers_file}"
 
-echo "[*] Git log show signature: with John's public key added to the allowed signers file"
+echo "[+] Git log show signature: with John's public key added to the allowed signers file"
 # Good "git" signature for john.doe@example.com with ED25519 key SHA256:...
 cd "${demo_repo_dir}"
 git log --show-signature -2
+
+echo "[*] === Process GitHub GPG signed commits ==="
+echo "[+] Git log show signature: without GitHub signing key"
+first_commit="$(git rev-list --max-parents=0 HEAD)"
+github_fingerprint="968479A1AFF927E37D1A566BB5690EEEBB952194"
+# for demonstration purposes the GitHub GPG public signing key is removed from
+# the local keyring to show the difference in the output when it is added later
+if gpg --list-public-keys |& grep --quiet "${github_fingerprint}"; then
+  echo "[+] Delete GitHub GPG public signing key from the local keyring"
+  gpg --batch --quiet --delete-key "${github_fingerprint}"
+fi
+git log --show-signature "${first_commit}"
+
+echo "[+] Add GitHub GPG public signing key to the keyring"
+cd "${repo_root_dir}"
+gpg --quiet --import < web-flow.gpg
+echo "[+] Set GitHub owner trust to ultimate"
+echo "${github_fingerprint}:6:" | gpg --import-ownertrust --quiet
+
+echo "[+] Git log show signature: with GitHub GPG public signing key added to the keyring"
+git log --show-signature "${first_commit}"
